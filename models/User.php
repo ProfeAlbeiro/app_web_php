5000c68f667a016bb04a4fbfd8f8d7527f07c000
ProfeAlbeiro <profealbeiro2020@gmial.com>
2025-02-01 15:17:23 -0500

    pasos: 1, 2, 3

---
+++ models/User.php
@@ -0,0 +1,21 @@
+<?php
+
+    class User{
+        // Atributos
+        private $user_id;
+        private $user_lastname;
+        private $user_name;
+        private $user_birthday;
+        private $user_gender;
+        private $user_maritalstatus;
+        private $user_age;
+        private $user_address;
+        private $user_localphone;
+        private $user_movil;
+        private $user_email;
+        private $user_profession;
+
+
+    }
+
+?>
