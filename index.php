aecfb4c0be9295cfa34ad0b53b831b5d926fdfe4
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-02-08 14:33:28 -0500

    paso 4. Modelo de Conexión y la BBDD

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+    require_once "models/DataBase.php";
+    $prueba = DataBase::connection();
     require_once "controllers/Landing.php";
     $controller = new Landing;
     $controller->main();
