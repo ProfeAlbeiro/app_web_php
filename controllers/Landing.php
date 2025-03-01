62bd06de1131ac7222f84cb554a83f1bb84f68a9
ProfeAlbeiro <profealbeiro2020@gmail.com>
2025-03-01 15:08:14 -0500

    7. Implementación de las vistas + Validación Front-Ent, primera parte

--- controllers/Landing.php
+++ controllers/Landing.php
@@ -4,16 +4,29 @@
         
         // Métodos
         public function main(){
+            require_once "views/company/header.view.php";
             require_once "views/company/index.view.php";
+            require_once "views/company/footer.view.php";
         }
         
+        // Formulario acerca de Nosotros
+        public function about(){
+            require_once "views/company/header.view.php";
+            require_once "views/company/about.view.php";
+            require_once "views/company/footer.view.php";
+        }
+        // Formulario de Registro 
+        public function shop(){
+            require_once "views/company/header.view.php";
+            require_once "views/company/shop.view.php";
+            require_once "views/company/footer.view.php";
+        }
         // Formulario de Registro 
         public function register(){
+            require_once "views/company/header.view.php";
             require_once "views/company/register.view.php";
-        }
-        
-        public function goodJob(){
-            require_once "views/company/correct.view.php";
+            require_once "views/company/footer.view.php";
         }
     }
+       
 ?>
